7471958e1718ab78cc20b8c6d9d93fe31724144c
Marko Goronjic <marko.goronjic@quantox.com>
2023-01-15 13:58:44 +0100

    Adding Styles

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,15 @@ import Link from "next/link";
 export default function Home() {
     return (
      <div>
-         <Navbar/>
-         <h1>Homepage</h1>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
-         <Link href='/ninjas'>See Ninja Listing</Link>
-        <Footer/>
+         <h1 className={styles.title}>Homepage</h1>
+         <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+         <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+         <Link
+             href="/ninjas"
+             className={styles.btn}
+         >
+             See Ninja Listing
+         </Link>
      </div>
     )
 }
