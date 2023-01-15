214628cff5de2cfa208021342e3dcbbb2a68c321
Marko Goronjic <marko.goronjic@quantox.com>
2023-01-15 11:50:57 +0100

    Linking Between Pages

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Link from "next/link";
 
 export default function Home() {
     return (
@@ -10,6 +11,7 @@ export default function Home() {
          <h1>Homepage</h1>
          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+         <Link href='/ninjas'>See Ninja Listing</Link>
         <Footer/>
      </div>
     )
