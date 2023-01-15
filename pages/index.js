9512e86f90fe1f61c7d74d128bada167f3013372
Marko Goronjic <marko.goronjic@quantox.com>
2023-01-15 11:36:15 +0100

    Adding Other Components

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function Home() {
     return (
      <div>
+         <Navbar/>
          <h1>Homepage</h1>
          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+        <Footer/>
      </div>
     )
 }
