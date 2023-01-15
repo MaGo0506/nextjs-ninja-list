6e92616b9a214cbe7105bb7c43ad84279ad6dcf8
Marko Goronjic <marko.goronjic@quantox.com>
2023-01-15 15:07:18 +0100

    Images & Metadata

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,16 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
     return (
         <nav>
             <div className="logo">
-                <h1>Ninja List</h1>
+                <Image
+                    src="/net-ninja.png" 
+                    width={128} 
+                    height={77}
+                    alt="ninja list logo"
+                />
             </div>
             <Link href='/'>Home</Link>
             <Link href='/about'>About</Link>
