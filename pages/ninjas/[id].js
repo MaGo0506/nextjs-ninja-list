535425a2b76203baa12347b550e0d0665a9ddb08
Marko Goronjic <marko.goronjic@quantox.com>
2023-01-15 16:05:48 +0100

    Fetching a Single Item

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,3 +1,6 @@
+import Home from "@/pages";
+import Head from "next/head";
+
 export const getStaticPaths = async () => {
     const res = await fetch(`https://jsonplaceholder.typicode.com/users`)
     const data = await res.json();
@@ -14,11 +17,31 @@ export const getStaticPaths = async () => {
     }
 }
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+    const id = context.params.id;
+
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    const data = await res.json();
+
+    return {
+        props: {ninja: data}
+    }
+}
+
+const Details = ({ninja}) => {
     return (
-        <div>
-            <h1>Details Page</h1>
-        </div>
+        <>
+            <Head>
+                <title>Ninja List | {ninja.username}</title>
+                <meta name="keywords" content="ninja"/>
+            </Head>
+            <div>
+                <h1>{ninja.name}</h1>
+                <p>{ninja.email}</p>
+                <p>{ninja.website}</p>
+                <p>{ninja.address.city}</p>
+            </div>
+        </>
     );
 }
 
